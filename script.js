d1b41feab4abfb19d8d7f6df8044acbbff86a381
vavegap <vavp@hotmail.com>
2025-05-01 18:48:48 -0300

    agregue carpeta imagenes

---
+++ script.js
@@ -0,0 +1,6 @@
+const lista= document.getelementbyid("listaPersonas");
+const input= document.getelementbyid("apellisdoInput");
+const agregarBtn= document.getelementbyid("agregarBtn");
+const foto= document.getelementbyid("fotoPersona");
+
+let persona = [];
