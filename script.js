b95f523d523c3ff3293697f958f127a9f715f28e
vavegap <vavp@hotmail.com>
2025-05-04 19:38:21 -0300

    Version final

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-const lista= document.getelementbyid("listaPersonas");
-const input= document.getelementbyid("apellisdoInput");
-const agregarBtn= document.getelementbyid("agregarBtn");
-const foto= document.getelementbyid("fotoPersona");
+const lista = document.getElementById("listaPersonas");
+const input = document.getElementById("apellidoInput");
+const agregarBtn = document.getElementById("agregarBtn");
+const foto = document.getElementById("fotoPersona");
 
-let persona = [];
+let personas = [];
 
-agregarBtn.addEventListener("click",() => {
-     const apellido = input.value.trim();
+agregarBtn.addEventListener("click", () => {
+    const apellido = input.value.trim();
     if (apellido !== "") {
         personas.push(apellido);
         mostrarLista();
         input.value = "";
     }
 });
+
+function mostrarLista() {
+    lista.innerHTML = "";
+    personas.forEach((apellido, index) => {
+        const li = document.createElement("li");
+
+        const span = document.createElement("span");
+        span.textContent = apellido;
+        span.addEventListener("click", () => mostrarFoto(apellido));
+
+        const checkbox = document.createElement("input");
+        checkbox, type = "checkbox";
+
+        const editarBtn = document.createElement("button");
+        editarBtn.textContent = "Editar";
+        editarBtn.onclick = () => editarApellido(index);
+
+        const elinarBtn = document.createElement("button");
+        elinarBtn.textContent = "Eliminar";
+        elinarBtn.onclick = () => {
+            personas.splice(index, 1);
+            mostrarLista();
+            foto.style.display = "none";
+        };
+
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.appendChild(editarBtn);
+        li.appendChild(elinarBtn);
+
+        lista.appendChild(li);
+    });
+}
+
+function mostrarFoto(apellido) {
+    foto.src = `imagenes/${apellido.toLowerCase()}.jpg`;
+    foto.style.display = "block";
+}
+
+function editarApellido(index) {
+    const nuevoApellido = prompt("Editar apellido:", personas[index]);
+    if (nuevoApellido) {
+        personas[index] = nuevoApellido.trim();
+        mostrarLista();
+    }
+}
 
 
